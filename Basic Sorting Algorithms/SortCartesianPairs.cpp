d11139829eaae19d27de62566683e60faa9e5d50
Sudarsan Rajagopalan <sudarsan93@gmail.com>
2022-05-22 12:53:00 -0700

    Create SortCartesianPairs.cpp

---
+++ Basic Sorting Algorithms/SortCartesianPairs.cpp
@@ -0,0 +1,11 @@
+// Given co-ordinates of N points on X-Y axis, your task is to sort them in accordance to the x-coordinate. If the x-coordinates are same then compare y-coordinates.
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>> sortCartesian(vector<pair<int, int>> v)
+{
+    int N = v.size();
+    // your code goes here
+    sort(v.begin(), v.end());
+    return v;
+}
