c838f90623098cbb26407c9d7843b430c583b6b8
Sudarsan Rajagopalan <sudarsan93@gmail.com>
2022-06-07 12:51:03 -0700

    Create ModuloExponentiation.cpp

---
+++ Bit Manipulation/ModuloExponentiation.cpp
@@ -0,0 +1,30 @@
+//Expected Time Complexity : O(Log N)
+//Given three numbers x, y and mod, compute x raised to power y modulo mod i.e.  (x^y)%mod
+
+#include <iostream>
+using namespace std;
+ 
+ 
+int power(int x, int y, int mod)
+{
+    int ans = 1;
+    x = x % mod; //Update x if it is more than mod
+    
+    if(!x)
+        return 0; //if x is exactly divisible by mod then x would be 0 
+                  //therefore x^y will be 0.
+    
+    while(y > 0)
+    {
+        int last_bit = (y&1);
+        
+        if(last_bit)
+            ans = (ans*x) % mod;
+            
+        x = (x * x) % mod; 
+        y = y >> 1;
+        
+    }
+    
+    return ans;
+}
