01173a336fa023dc71c5386afbc40d9a023c2516
Sudarsan Rajagopalan <sudarsan93@gmail.com>
2022-06-07 12:52:14 -0700

    Update ModuloExponentiation.cpp

--- Bit Manipulation/ModuloExponentiation.cpp
+++ Bit Manipulation/ModuloExponentiation.cpp
@@ -1,6 +1,6 @@
 //Expected Time Complexity : O(Log N)
 //Given three numbers x, y and mod, compute x raised to power y modulo mod i.e.  (x^y)%mod
-//Doing Fast Exponenation - O(Log N) ie.  O(Log y)
+//Doing Fast Exponenation - O(Log N) ie.  O(Log y) in this case
 
 #include <iostream>
 using namespace std;
