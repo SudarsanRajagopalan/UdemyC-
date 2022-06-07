8ff64f883f417adc8a4a869dbd7f3b7a224108e2
Sudarsan Rajagopalan <sudarsan93@gmail.com>
2022-06-07 12:09:14 -0700

    Update Xoring.cpp

--- Bit Manipulation/Xoring.cpp
+++ Bit Manipulation/Xoring.cpp
@@ -1,3 +1,4 @@
+//We are given an array containg n numbers. All the numbers are present twice except for one number which is only present once. Find the unique number without taking any extra spaces and in linear time.
 //Expected Time Complexity : O(N)
 //XOR of two same elements is 0
 
